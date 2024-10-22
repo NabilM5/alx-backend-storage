a9d5eb4d5b90c9a6b5aef3febb8a85064d4151ce
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2024-10-23 01:40:24 +0300

    update

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,33 @@
+ #!/usr/bin/env python3
+"""
+script that provides some stats about
+Nginx logs stored in MongoDB
+"""
+
+from pymongo import MongoClient
+
+def log_stats():
+    """
+    Nginx logs stored in MongoDB
+    """
+    client = MongoClient('localhost', 27017)
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.count_documents({})
+
+    print(f"{total_logs} logs")
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    count_status = collection.count_documents({"method": "GET",
+                                                    "path": "/status"})
+    print(f"{count_status} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
