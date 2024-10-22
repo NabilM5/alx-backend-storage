9b87751dbb0e83da33e172bcc5aa7fb75e935f56
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2024-10-23 02:46:33 +0300

    update

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 """
-    Returns all students from the collection
-    ordered by average score.
+Returns a list of students from the MongoDB collection,
+ordered by their average score.
+
+:param mongo_collection: pymongo collection object
+:return: list of students with their average score added
+"""
+
+def top_students(mongo_collection):
+    """
+    Aggregates students' scores and calculates their average score.
 
     :param mongo_collection: pymongo collection object
     :return: list of students with their average score added
-"""
-
-
-def top_students(mongo_collection):
+    """
+    
+    # Define the aggregation pipeline to calculate average scores
     pipeline = [
         {
             "$addFields": {
@@ -19,10 +26,13 @@ def top_students(mongo_collection):
         },
         {
             "$sort": {
-                "averageScore": -1
+                "averageScore": -1  # Sort by averageScore in descending order
             }
         }
     ]
 
+    # Execute the aggregation pipeline on the collection
     results = mongo_collection.aggregate(pipeline)
+
+    # Convert results to a list and return
     return list(results)
