2fd9763d6b2150335634f102088508b47fb24c68
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2024-10-23 17:22:25 +0000

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""
+Caching request module
+"""
 import redis
 import requests
 from functools import wraps
 from typing import Callable
-import time
 
 
-redis_store = redis.Redis(decode_responses=True)
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
 
 
-def data_cacher(method: Callable[[str], str]) -> Callable[[str], str]:
-    '''Decorator to cache the output of the fetched data.
-    '''
-    @wraps(method)
-    def invoker(url: str) -> str:
-        '''The wrapper function for caching the output.
-        Increments the access count for the given URL and caches its result.
-        '''
-        # Increment the count for the URL access
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result is not None:
-            return result
-        result = method(url)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
+@track_get_page
 def get_page(url: str) -> str:
-    '''Fetches the HTML content of a URL after caching the request's response,
-    and tracks the request.
-    '''
-    return requests.get(url).text
-
-
-if __name__ == "__main__":
-    url = "http://google.com"
-    print("First call (cache store):", get_page(url))
-    print("Second call (cache hit):", get_page(url))
-    time.sleep(11)
-    print("Third call (cache expired):", get_page(url))
+    """ Makes a http request to a given endpoint
+    """
+    response = requests.get(url)
+    return response.text
