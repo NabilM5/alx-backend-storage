5e64608962ac94144541413f1d5fb172c95a4b01
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2024-10-29 02:58:54 +0300

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,36 +6,29 @@ import requests
 from functools import wraps
 from typing import Callable
 
+
 redis_store = redis.Redis()
 '''The module-level Redis instance.
 '''
 
+
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data and tracks request counts.'''
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def invoker(url: str) -> str:
-        '''The wrapper function for caching the output and tracking the request.'''
-        
-        # Increment count for the URL and ensure the count is correct
-        if not redis_store.exists(f'count:{url}'):
-            redis_store.set(f'count:{url}', 0)
-        count_incremented = redis_store.incr(f'count:{url}')
-        if count_incremented is None:
-            return "Error: Count increment failed"
-        
-        # Check if there's a cached result
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
-        
-        # Fetch and cache the result if not already cached
         result = method(url)
-        
-        # Set cache expiry for the result at 10 seconds
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
-        
         return result
     return invoker
+
 
 @data_cacher
 def get_page(url: str) -> str:
